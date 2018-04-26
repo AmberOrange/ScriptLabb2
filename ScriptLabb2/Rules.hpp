6e7453fc7df6947c4a8ed1b72142cf7e95ab12c6
Emil <Emil@EMIL-DESKTOP>
2018-04-26 19:51:32 +0200

    Implemented the rest

--- ScriptLabb2/Rules.hpp
+++ ScriptLabb2/Rules.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include "Regex.hpp"
+#include <list>
+#include <iostream>
 /*
 NUM : "[0-9][0-9]*"
 HEX : "0x[0-9a-fA-F][0-9a-fA-F]*"
@@ -10,56 +12,245 @@ TABLE : "{" FIELDRULE "}"
 FIELDRULE: FIELD FIELDRULE2 | EMPTY
 FIELDRULE2: DELIM FIELDRULE | EMPTY
 FIELD: ASSIGN | VALUE
-VALUE: HEX | NUM | WORD | TABLE | STR
+VALUE: HEX | NUM | VAR |  STR | TABLE
 ASSIGN: ASSIGNKEY "=" VALUE
 ASSIGNKEY: VAR | "[" KEY "]"
 KEY: NUM | STR
 */
+
+class Tree
+{
+public:
+	std::string lexeme, tag;
+	std::list<Tree*> children;
+	Tree(std::string t, const char *l, int size)
+		: tag(t), lexeme(l, size) {}
+	~Tree()
+	{
+		for (Tree *n : this->children)
+			if (n != nullptr) delete n;
+	}
+	void dump(int depth = 0)
+	{
+		for (int i = 0; i<depth; i++)
+			std::cout << "  ";
+		std::cout << this->tag << ": " << this->lexeme << std::endl;
+		for (Tree *n : this->children)
+			if(n != nullptr) n->dump(depth+1);
+		// Recurse over the children
+	}
+};
+
 namespace rule {
+	bool NUM(const char **text, Tree **result);
+	bool HEX(const char **text, Tree **result);
+	bool VAR(const char **text, Tree **result);
+	bool STR(const char **text, Tree **result);
+	bool DELIM(const char **text, Tree **result);
+	bool TABLE(const char **text, Tree **result);
+	bool TERM(char term, const char **text, Tree **result);
+	bool FIELDRULE(const char **text, Tree **result);
+	bool FIELDRULE2(const char **text, Tree **result);
+	bool FIELD(const char **text, Tree **result);
+	bool VALUE(const char **text, Tree **result);
+	bool ASSIGN(const char **text, Tree **result);
+	bool ASSIGNKEY(const char **text, Tree **result);
+	bool KEY(const char **text, Tree **result);
+
 	// NUM : "[0-9][0-9]*"
-	int NUM(char const *text)
+	bool NUM(const char **text, Tree **result)
 	{
 		static CharClass num("0123456789");
 		static Star numStar(&num);
-		const static Seq seq({ &num, &numStar });
-		return seq.match(text);
+		static Seq seq({ &num, &numStar });
+		int length = seq.match(*text);
+		if (length == -1) return false;
+		*result = new Tree("NUM", *text, length);
+		*text += length;
+		return true;
 	}
 	// HEX : "0x[0-9a-fA-F][0-9a-fA-F]*"
-	int HEX(char const *text)
+	bool HEX(const char **text, Tree **result)
 	{
 		static CharClass num("0123456789abcdefABCDEF");
 		static Star numStar(&num);
 		static CharClass zero("0");
 		static CharClass x("x");
-		const static Seq seq({ &zero, &x, &num, &numStar });
-		return seq.match(text);
+		static Seq seq({ &zero, &x, &num, &numStar });
+		int length = seq.match(*text);
+		if (length == -1) return false;
+		*result = new Tree("HEX", *text, length);
+		*text += length;
+		return true;
 	}
 	// VAR : "[a-zA-Z][a-zA-Z]*"
-	int VAR(char const *text)
+	bool VAR(const char **text, Tree **result)
 	{
 		static CharClass cha("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 		static Star chaStar(&cha);
-		const static Seq seq({ &cha, &chaStar });
-		return seq.match(text);
+		static Seq seq({ &cha, &chaStar });
+		int length = seq.match(*text);
+		if (length == -1) return false;
+		*result = new Tree("VAR", *text, length);
+		*text += length;
+		return true;
 	}
 	// STR : "\"[a-zA-Z]*\""
-	int STR(char const *text)
+	bool STR(const char **text, Tree **result)
 	{
 		static CharClass cha("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ");
 		static Star chaStar(&cha);
 		static CharClass cite("\"");
-		const static Seq seq({ &cite, &chaStar, &cite });
-		return seq.match(text);
+		static Seq seq({ &cite, &chaStar, &cite });
+		int length = seq.match(*text);
+		if (length == -1) return false;
+		*result = new Tree("STR", *text, length);
+		*text += length;
+		return true;
 	}
 	// DELIM : "[,;]"
-	int DELIM(char const *text)
+	bool DELIM(const char **text, Tree **result)
 	{
 		static CharClass delim(",;");
-		return delim.match(text);
+		int length = delim.match(*text);
+		if (length == -1) return false;
+		*result = new Tree("DELIM", *text, length);
+		*text += length;
+		return true;
 	}
-	// TABLE : "{" FIELDRULE "}"
-	bool TABLE(char const *text)
-	{
 
+	bool TERM(char term, const char **text, Tree **result)
+	{
+		if (**text == term)
+		{
+			*result = new Tree("TERM", *text, 1);
+			(*text)++;
+			return true;
+		}
+		else return false;
+	}
+
+	// TABLE : "{" FIELDRULE "}"
+	bool TABLE(const char **text, Tree **result)
+	{
+		Tree *child[3] = { nullptr };
+		const char *backup = *text;
+		if (!TERM('{', text, &child[0])) return false;
+		if (!FIELDRULE(text, &child[1])) return false;
+		if (!TERM('}', text, &child[2])) return false;
+		*result = new Tree("TABLE", backup, *text - backup);
+		(*result)->children.push_back(child[0]);
+		(*result)->children.push_back(child[1]);
+		(*result)->children.push_back(child[2]);
+		return true;
+	}
+	//FIELDRULE: FIELD FIELDRULE2 | EMPTY
+	bool FIELDRULE(const char **text, Tree **result)
+	{
+		Tree *child[2] = { nullptr };
+		const char *backup = *text;
+		if (FIELD(text, &child[0]) && FIELDRULE2(text, &child[1]))
+		{
+			*result = new Tree("FIELDRULE", backup, *text - backup);
+			(*result)->children.push_back(child[0]);
+			(*result)->children.push_back(child[1]);
+		}
+		else
+			*text = backup;
+		return true;
+	}
+	//FIELDRULE2 : DELIM FIELDRULE | EMPTY
+	bool FIELDRULE2(const char **text, Tree **result)
+	{
+		Tree *child[2] = { nullptr };
+		const char *backup = *text;
+		if (DELIM(text, &child[0]) && FIELDRULE(text, &child[1]))
+		{
+			*result = new Tree("FIELDRULE2", backup, *text - backup);
+			(*result)->children.push_back(child[0]);
+			(*result)->children.push_back(child[1]);
+		} else
+			*text = backup;
+		return true;
+	}
+	//FIELD : ASSIGN | VALUE
+	bool FIELD(const char **text, Tree **result)
+	{
+		Tree *child = nullptr;
+		const char *backup = *text;
+		if (ASSIGN(text, &child) || VALUE(text, &child))
+		{
+			*result = new Tree("FIELD", backup, *text - backup);
+			(*result)->children.push_back(child);
+			return true;
+		}
+		return false;
+	}
+	//VALUE: HEX | NUM | VAR | STR | TABLE
+	bool VALUE(const char **text, Tree **result)
+	{
+		Tree *child = nullptr;
+		const char *backup = *text;
+		if (HEX(text, &child)
+			|| NUM(text, &child)
+			|| VAR(text, &child)
+			|| STR(text, &child)
+			|| TABLE(text, &child))
+		{
+			*result = new Tree("VALUE", backup, *text - backup);
+			(*result)->children.push_back(child);
+			return true;
+		}
+		return false;
+	}
+	//ASSIGN : ASSIGNKEY "=" VALUE
+	bool ASSIGN(const char **text, Tree **result)
+	{
+		Tree *child[3] = { nullptr };
+		const char *backup = *text;
+		if (!ASSIGNKEY(text, &child[0])) return false;
+		if (!TERM('=', text, &child[1])) return false;
+		if (!VALUE(text, &child[2])) return false;
+		*result = new Tree("ASSIGN", backup, *text - backup);
+		(*result)->children.push_back(child[0]);
+		(*result)->children.push_back(child[1]);
+		(*result)->children.push_back(child[2]);
+		return true;
+	}
+	//ASSIGNKEY : VAR | "[" KEY "]"
+	bool ASSIGNKEY(const char **text, Tree **result)
+	{
+		const char *backup = *text;
+		Tree *child1 = nullptr;
+		if (VAR(text, &child1))
+		{
+			*result = new Tree("ASSIGNKEY", backup, *text - backup);
+			(*result)->children.push_back(child1);
+			return true;
+		}
+		else {
+			Tree *child[3] = { nullptr };
+			if (!(TERM('[', text, &child[0]))) return false;
+			if (!KEY(text, &child[1])) return false;
+			if (!TERM(']', text, &child[2])) return false;
+			*result = new Tree("ASSIGNKEY", backup, *text - backup);
+			(*result)->children.push_back(child[0]);
+			(*result)->children.push_back(child[1]);
+			(*result)->children.push_back(child[2]);
+			return true;
+		}
+	}
+	//KEY : NUM | STR
+	bool KEY(const char **text, Tree **result)
+	{
+		Tree *child = nullptr;
+		const char *backup = *text;
+		if (NUM(text, &child) || STR(text, &child))
+		{
+			*result = new Tree("KEY", backup, *text - backup);
+			(*result)->children.push_back(child);
+			return true;
+		}
+		return false;
 	}
 }
--- ScriptLabb2/main.cpp
+++ ScriptLabb2/main.cpp
@@ -3,8 +3,25 @@
 #include <iostream>
 #include <string>
 
+
+
+void ValidateExpression(const char* text)
+{
+	const char* backup = text;
+	Tree *child;
+	bool result = rule::TABLE(&text, &child);
+	std::cout << backup << " : "
+		<< (result ? "True" : "False") << std::endl;
+
+	if (result)
+	{
+		child->dump();
+		delete child;
+	}
+	std::cout << "\n\n\n";
+}
+
 int main() {
-	//const char *test = "0541543x4323";
 	std::string testStrings[] =
 	{
 		"0x1",
@@ -25,64 +42,20 @@ int main() {
 		std::cout << s << " " << (Compare(&custom, s) ? "True" : "False") << std::endl;
 	}
 
-	//std::cout << *(test+1);
+	std::cout << std::endl << std::endl;
+
+	ValidateExpression("{}");
+	ValidateExpression("{1,2;3}");
+	ValidateExpression("{1,2;3,}");
+	ValidateExpression("{easyas=\"abc\";2;2,[\"hello\"]=\"world\",[3]=4}");
+	ValidateExpression("{{1,2,3},data={0x77}}");
+	ValidateExpression("{{}");
+	ValidateExpression("{;}");
+	ValidateExpression("{1,,}");
+	ValidateExpression("{34=7}");
+	ValidateExpression("{alpha=beta=gamma}");
+
+
 	std::cin.ignore();
 	return 0;
 }
-
-
-// TABLE : "{" TABLE2 "}"
-// TABLE2 : "gs" TABLE2 | empty
-/*
-{gsgsgsgsgsgsgswqe}
-
-TABLE: {gsgsgsgsgsgsgswqe}
-	TERM: {
-	TABLE2: gsgsgsgsgsgsgswqe}
-		TERM: gs
-		TABLE2: gsgsgsgsgsgswqe}
-			TABLE2: gsgsgsgsgs
-				...
-					TABLE2: wqe}
-	TERM: }
-
-
-
-
-
-{}
-TABLE : {}
-	TERM: {
-	TABLE2: }
-		Empty
-	TERM: }
-
-NUM : "[0-9][0-9]*"
-HEX : "0x[0-9a-fA-F][0-9a-fA-F]*"
-VAR : "[a-zA-Z][a-zA-Z]*"
-STR : "\"[a-zA-Z]*\""
-DELIM : "[,;]"
-TABLE : "{" FIELDRULE "}"
-FIELDRULE: FIELD FIELDRULE2 | EMPTY
-FIELDRULE2: DELIM FIELDRULE | EMPTY
-FIELD: ASSIGN | VALUE
-VALUE: HEX | NUM | WORD | TABLE | STR 
-ASSIGN: ASSIGNKEY "=" VALUE
-ASSIGNKEY: VAR | "[" KEY "]"
-KEY: NUM | STR
-
-{1,2;3}
-TABLE : {1,2;3}
-	TERM: {
-	TABLE2 : 1,2;3}
-		NUMRULE : 1,
-			NUM : 1
-			DELIM : ,
-		EMPTY
-		NUMRULE : 2;
-			NUM : 2
-			DELIM : ;
-		EMPTY
-
-
-*/
